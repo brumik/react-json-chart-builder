83d1eb54bb30bbb9ad77246163e0f9b542cac9a2
brumik <brumik@users.noreply.github.com>
2021-05-27 11:33:17 +0000

    Deploying to gh-pages from @ brumik/react-json-chart-builder@b233a17c7411e83bf27a9947a4f93ca69ccac16a 🚀

---
+++ api/index.ts
@@ -0,0 +1,13 @@
+import anomaly from './anomaly';
+import demoByOrgPie from './demoByOrgPie';
+import demoByTimeGrouped from './demoByTimeGrouped';
+import demoByTimeStacked from './demoByTimeStacked';
+import groupedStacked from './groupedStacked';
+
+export default {
+    anomaly,
+    demoByOrgPie,
+    demoByTimeGrouped,
+    demoByTimeStacked,
+    groupedStacked
+};
