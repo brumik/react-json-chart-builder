cfec947899e6c09a57fc0555218359880e287777
brumik <brumik@users.noreply.github.com>
2021-06-02 14:27:44 +0000

    Deploying to gh-pages from @ brumik/react-json-chart-builder@7cb88d6debfcf9c04be376eccdd3d52d0993b416 🚀

--- api/groupedStacked.ts
+++ api/groupedStacked.ts
@@ -6,36 +6,36 @@ export default {
             'total_hours_spent_risk_adjusted': 126,
             'total_hours_saved': 0,
             'cumulative_time_net_benefits': -126,
-            'total_costs': 7560,
+            'total_costs': -75,
             'total_benefits': 0,
-            'cumulative_net_benefits': -7560
+            'cumulative_net_benefits': -75
         },
         {
             'year': 'year1',
             'total_hours_spent_risk_adjusted': 126,
             'total_hours_saved': 1140,
             'cumulative_time_net_benefits': 888,
-            'total_costs': 7560,
-            'total_benefits': 68400,
-            'cumulative_net_benefits': 53280
+            'total_costs': -75,
+            'total_benefits': 68,
+            'cumulative_net_benefits': 53
         },
         {
             'year': 'year2',
             'total_hours_spent_risk_adjusted': 6,
             'total_hours_saved': 1311,
             'cumulative_time_net_benefits': 2193,
-            'total_costs': 360,
-            'total_benefits': 78660,
-            'cumulative_net_benefits': 131580
+            'total_costs': -36,
+            'total_benefits': 78,
+            'cumulative_net_benefits': 13
         },
         {
             'year': 'year3',
             'total_hours_spent_risk_adjusted': 6,
             'total_hours_saved': 1508,
             'cumulative_time_net_benefits': 3695,
-            'total_costs': 360,
-            'total_benefits': 90459,
-            'cumulative_net_benefits': 221679
+            'total_costs': -36,
+            'total_benefits': 90,
+            'cumulative_net_benefits': 22
         }
     ]
 }
