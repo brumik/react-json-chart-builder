8fe9c7995b14de5a4ddc208557836008a963d65f
Levente Berky <levente.berky@gmail.com>
2021-06-09 14:42:07 +0200

    fix(export): Exported grouped and non grouped api interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './components/types';
 export { default as default } from './components/index';
 export { default as functions } from './components/Functions/';
-export { ApiReturnType, ApiType } from './components/Api/';
+export * from './components/Api/';
