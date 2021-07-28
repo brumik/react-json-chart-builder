89dc3ea5ccabced4b631818f8e8f6412f42567ae
knightsean00 <knightsean00@gmail.com>
2021-07-28 13:57:59 -0700

    fix(formatNumberAsK): Skip formatting for whole numbers under 1000

--- demo/api/groupedStacked.ts
+++ demo/api/groupedStacked.ts
@@ -6,36 +6,36 @@ export default {
             'total_hours_spent_risk_adjusted': 126,
             'total_hours_saved': 0,
             'cumulative_time_net_benefits': -126,
-            'total_costs': -534234,
-            'total_benefits': 250349,
-            'cumulative_net_benefits': -283885
+            'total_costs': -250349,
+            'total_benefits': 534234,
+            'cumulative_net_benefits': 283885
         },
         {
             'year': 'year1',
             'total_hours_spent_risk_adjusted': 126,
             'total_hours_saved': 1140,
             'cumulative_time_net_benefits': 888,
-            'total_costs': -2140010,
-            'total_benefits': 1000010,
-            'cumulative_net_benefits': -1423885
+            'total_costs': -1000010,
+            'total_benefits': 2140010,
+            'cumulative_net_benefits': 1423885
         },
         {
             'year': 'year2',
             'total_hours_spent_risk_adjusted': 6,
             'total_hours_saved': 1311,
             'cumulative_time_net_benefits': 2193,
-            'total_costs': -13420,
-            'total_benefits': 749383,
-            'cumulative_net_benefits': -687922
+            'total_costs': -749383,
+            'total_benefits': 13420,
+            'cumulative_net_benefits': 687922
         },
         {
             'year': 'year3',
             'total_hours_spent_risk_adjusted': 6,
             'total_hours_saved': 1508,
             'cumulative_time_net_benefits': 3695,
-            'total_costs': -1405,
-            'total_benefits': 430284,
-            'cumulative_net_benefits': -259043
+            'total_costs': -430284,
+            'total_benefits': 1405,
+            'cumulative_net_benefits': 259043
         }
     ]
 }
