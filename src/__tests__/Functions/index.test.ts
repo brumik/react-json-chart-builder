12e3fc199ddf136d2219d1462e72197d8af5ac0d
Levente Berky <levente.berky@gmail.com>
2021-11-22 14:34:00 +0100

    chore(tests): Fix up the tests which were failing

--- src/__tests__/Functions/index.test.ts
+++ src/__tests__/Functions/index.test.ts
@@ -3,7 +3,7 @@ import functions from '../../components/Functions/';
 describe('Chart/Functions/index', () => {
     test('should return an object of functions with 3 entries', () => {
         expect(Object.keys(functions)).toEqual([
-            'onClick', 'axisFormat', 'fetchFnc'
+            'onClick', 'axisFormat', 'labelFormat', 'fetchFnc'
         ]);
     })
 });
--- src/__tests__/Functions/onClick.test.ts
+++ src/__tests__/Functions/onClick.test.ts
@@ -1,13 +1,12 @@
-import { consoleLogValue } from '../../components/Functions/onClick';
+import onClick from '../../components/Functions/onClick';
 
 describe('Chart/Functions/onClik', () => {
     test('should console log the on click params', () => {
         console.log = jest.fn();
-        const ev = { param: 1, param2: 2 };
         const params = { param3: 3, param4: 4 };
 
-        consoleLogValue(ev, params);
+        onClick.consoleLog(null, params);
 
-        expect(console.log).toHaveBeenCalledWith('onclick', ev, params);
+        expect(console.log).toHaveBeenCalledWith('onclick', null, params);
     })
 });
