252279d8981a43a1d23133dc64ae0a97f6a9a621
Levente Berky <levente.berky@gmail.com>
2021-11-29 14:19:26 +0100

    chore(demo): Change to hash router

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,7 +1,7 @@
 import '@patternfly/react-core/dist/styles/base.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const RootApp: React.FC<Record<string, never>> = () => (
