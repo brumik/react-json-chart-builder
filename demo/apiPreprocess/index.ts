f723a199c81d49a2417365d1a3e2471a9067881e
Levente Berky <levente.berky@gmail.com>
2021-12-01 14:33:41 +0100

    chore(demo): Fixed some code pretty problems

--- demo/apiPreprocess/index.ts
+++ demo/apiPreprocess/index.ts
@@ -3,66 +3,65 @@ import { ChartData, ChartDataSerie } from '../../src/components/types';
 import { ApiReturnType, ApiType, GroupedApi } from './types';
 
 export const convertGroupedByData = (data: GroupedApi): ChartDataSerie[] => {
-    const { dates } = data;
-    const items: ChartDataSerie[] = [];
-    dates.forEach((el) => {
-        // Add items to the correct serie
-        el.items.forEach((item, idx) => {
-            if (!items[idx]) {
-                items[idx] = {
-                    serie: [],
-                    hidden: false,
-                    name: uuidv4()
-                };
-            }
-            items[idx].serie.push({
-                created_date: el.date,
-                ...item
-            });
-        })
-    });
-    return items;
+  const { dates } = data;
+  const items: ChartDataSerie[] = [];
+  dates.forEach((el) => {
+    // Add items to the correct serie
+    el.items.forEach((item, idx) => {
+      if (!items[idx]) {
+        items[idx] = {
+          serie: [],
+          hidden: false,
+          name: uuidv4()
+        };
+      }
+      items[idx].serie.push({
+        created_date: el.date,
+        ...item
+      });
+    })
+  });
+  return items;
 }
 
 export const convertApiToData = (
-    result: ApiReturnType
+  result: ApiReturnType
 ): ChartData => {
-    const resolvedData: ChartData = {
-        series: [],
-        legend: []
-    };
+  const resolvedData: ChartData = {
+    series: [],
+    legend: []
+  };
 
-    // eslint-disable-next-line @typescript-eslint/dot-notation
-    if (result['dates']) {
-        result.type = ApiType.grouped
-    } else {
-        result.type = ApiType.nonGrouped
-    }
+  if ('dates' in result) {
+    result.type = ApiType.grouped
+  } else {
+    result.type = ApiType.nonGrouped
+  }
 
-    switch (result.type) {
-        case ApiType.grouped:
-            resolvedData.series = convertGroupedByData(result);
-            break;
-        case ApiType.nonGrouped:
-            resolvedData.series = [{
-                serie: result.items,
-                hidden: false,
-                name: uuidv4()
-            }];
-            break;
-    }
+  switch (result.type) {
+    case ApiType.grouped:
+      resolvedData.series = convertGroupedByData(result);
+      break;
+    case ApiType.nonGrouped:
+      resolvedData.series = [{
+        serie: result.items,
+        hidden: false,
+        name: uuidv4()
+      }];
+      break;
+  }
 
-    if (result.meta?.legend) {
-        resolvedData.legend = result.meta.legend.map((item) => {
-            const s = resolvedData.series.find(({ serie }) => {
-                return serie.find(({ id: serieId }) => serieId === item.id);
-            });
-            return {
-                ...item,
-                childName: s.name
-            };
-        });
-    }
+  if (result.meta?.legend) {
+    resolvedData.legend = result.meta.legend.map((item) => {
+      const s = resolvedData.series.find(({ serie }) => {
+        return serie.find(({ id: serieId }) => serieId === item.id);
+      });
+      return {
+        ...item,
+        childName: s.name
+      };
+    });
+  }
 
-    return resolvedData;
+  return resolvedData;
 };
